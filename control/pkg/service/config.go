758a07955abda70370b3b11c8da013c81261d6b6
도그푸터 <dogfooter.work@gmail.com>
2019-08-13 19:19:39 +0900

    fix

--- control/pkg/service/config.go
+++ control/pkg/service/config.go
@@ -158,7 +158,9 @@ func GetConfigUpdateFileList() UpdateFileList {
 	files := UpdateFileList{}
 	file, _ := ioutil.ReadFile(os.Getenv("DOGFOOTER_HOME") + "config/update_file_list.json")
 
-	_ = json.Unmarshal(file, &files)
+	if err := json.Unmarshal(file, &files); err != nil {
+		fmt.Fprintf(os.Stderr, "err=%v\n", err)
+	}
 
 	fmt.Fprintf(os.Stderr, "DEBUG: %#v\n", files)
 
